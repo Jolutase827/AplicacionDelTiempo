08993d3e47d32f43a8d9118245db5f9b675cdb80
Jose Luis <jolutase104@gmail.com>
2023-04-18 21:13:54 +0200

    18/04/2023

--- app/src/main/java/com/example/myweatherbase/activities/model/AnyadirCiudad.java
+++ app/src/main/java/com/example/myweatherbase/activities/model/AnyadirCiudad.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myweatherbase.R;
+
 public class AnyadirCiudad extends AppCompatActivity {
 
     @Override
